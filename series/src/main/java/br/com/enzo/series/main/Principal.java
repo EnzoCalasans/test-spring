f45dc48bdc5e2ea2b081797243ff1051d8aeaa83
Enzo <Enzocalasanscardosocorreacosta@gmail.com>
2025-05-21 18:37:21 -0300

    manipulando coleçao de dados

--- series/src/main/java/br/com/enzo/series/main/Principal.java
+++ series/src/main/java/br/com/enzo/series/main/Principal.java
@@ -3,12 +3,17 @@ package br.com.enzo.series.main;
 import br.com.enzo.series.model.DadosEpisodio;
 import br.com.enzo.series.model.DadosSerie;
 import br.com.enzo.series.model.DadosTemporada;
+import br.com.enzo.series.model.Episodio;
 import br.com.enzo.series.service.ConsumoAPI;
 import br.com.enzo.series.service.ConverteDados;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class Principal {
     private Scanner leitura = new Scanner(System.in);
@@ -34,14 +39,39 @@ public class Principal {
         }
         temporadas.forEach(System.out::println);
 
-//        for (int i = 0; i < dados.totalTemporadas(); i++){
-//            List<DadosEpisodio> episodiosTemporada = temporadas.get(i).episodios();
-//            for (int j = 0; j < episodiosTemporada.size(); j++){
-//                System.out.println(episodiosTemporada.get(j).titulo());
-//            }
-//        }
-
         temporadas.forEach(t -> t.episodios().forEach(e -> System.out.println(e.titulo())));
-        temporadas.forEach(System.out::println);
+
+        List<DadosEpisodio> dadosEpisodios = temporadas.stream()
+                .flatMap(t -> t.episodios().stream())
+                .collect(Collectors.toList());
+
+        System.out.println("Top 5 episodios");
+        dadosEpisodios.stream()
+                .filter(e -> !e.avaliacao().equalsIgnoreCase("N/A"))
+                .sorted(Comparator.comparing(DadosEpisodio::avaliacao).reversed())
+                .limit(5)
+                .forEach(System.out::println);
+
+        List<Episodio> episodios = temporadas.stream()
+                .flatMap(t -> t.episodios().stream()
+                        .map(d -> new Episodio(t.numero(), d))
+                ).collect(Collectors.toList());
+
+        episodios.forEach(System.out::println);
+
+        System.out.println("Ano dos eps: ");
+        var ano = leitura.nextInt();
+        leitura.nextLine();
+
+        LocalDate dataBusca = LocalDate.of(ano, 1, 1);
+
+        DateTimeFormatter formatador = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+
+        episodios.stream()
+                .filter(e -> e.getDataLancamento() != null && e.getDataLancamento().isAfter(dataBusca))
+                .forEach(e -> System.out.println("Temporada: " + e.getTemporada() +
+                        "Episódio: " + e.getTitulo() +
+                        "Data lançamento: " + e.getDataLancamento().format(formatador)
+                        ));
     }
 }
